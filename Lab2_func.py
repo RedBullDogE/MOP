52768d0a5765c9889d9686e11fae21f9b6c7feb4
Andrew Krivonos <gsb06092000@gmail.com>
2019-03-24 18:21:01 +0200

    Move output functions to separate file

--- Lab2_func.py
+++ Lab2_func.py
@@ -108,31 +108,3 @@ def response_function(coeffs, variables):
     """
     y = coeffs[0] + sum([b * x for b, x in zip(coeffs[1:], variables)])
     return y
-
-
-def output_table(x_matrix, y_matrix, y_average):
-    """
-    Converting matrix of X, matrix of Y and list of average values of Y into
-    a string, that represents the table
-
-    :param x_matrix: matrix of X
-    :param y_matrix: matrix of Y
-    :param y_average: list of average values of Y
-    :return: string for visualizing information
-    """
-    title = "Planning matrix:\n"
-    column_names = ["X{}".format(i + 1) for i in range(len(x_matrix[0]))] + \
-                   ["Y{}".format(i + 1) for i in range(len(y_matrix[0]))] + \
-                   ["Yav"]
-    total_list = [x_matrix[i] + y_matrix[i] + [y_average[i]] for i in range(len(x_matrix))]
-
-    x_body_layout = len(x_matrix[0]) * "{:^8.5} " + "|| "
-    y_body_layout = len(y_matrix[0]) * "{:^8.5} " + "| "
-    y_av_body_layout = "{:^8.5}\n"
-    body_layout = x_body_layout + y_body_layout + y_av_body_layout
-    body = body_layout.format(*column_names)
-    for i in range(len(total_list)):
-        body += body_layout.format(*total_list[i])
-
-    res_str = title + body
-    return res_str
