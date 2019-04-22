3656e412106d27ebc0d2ac042f4a4dfcf5fa38d7
RedBullDog <gsb06092000@gmail.com>
2019-04-22 18:24:32 +0300

    Lab5 finished

--- Lab5.py
+++ Lab5.py
@@ -1,6 +1,8 @@
 from itertools import combinations, product
 from prettytable import PrettyTable
-
+from random import uniform
+import numpy as np
+from common_functions import dispersion, cohren, student, fisher, beta_value
 
 x1_min, x1_max = -2, 4
 x2_min, x2_max = -10, 8
@@ -8,8 +10,9 @@ x3_min, x3_max = -3, 6
 x_min, x_max = (x1_min + x2_min + x3_min) / 3, (x1_max + x2_max + x3_max) / 3
 y_min = 200 + x_min
 y_max = 200 + x_max
-x_min_list = [x1_min, x2_min, x3_min, x1_min * x2_min, x1_min * x3_min, x2_min * x3_min, x1_min * x2_min * x3_min]
-x_max_list = [x1_max, x2_max, x3_max, x1_max * x2_max, x1_max * x3_max, x2_max * x3_max, x1_max * x2_max * x3_max]
+x_min_list = [x1_min, x2_min, x3_min]
+x_max_list = [x1_max, x2_max, x3_max]
+x0_list = [(x_max_list[i] + x_min_list[i]) / 2 for i in range(len(x_max_list))]
 
 
 class Coefficient:
@@ -164,12 +167,39 @@ def mul(arr):
     res = 1
     for el in arr:
         res *= el
-    return res
+    return round(res, 3)
 
 
-def gen_norm_matrix(f_num, n=0, quadratic=False):
+def naturalize(min_val, max_val, val):
+    x0 = (max_val + min_val) / 2
+    dx = max_val - x0
+    return val * dx + x0
+
+
+def calc_coeffs(x_matrix, y_list):
+    features = list(zip(*x_matrix))
+    mx_list = [sum(features[i]) / len(features[i]) for i in range(len(features))]
+    a_ij_matrix = [
+        [
+            sum([x1 * x2 for x1, x2 in zip(features[i], features[j])]) / len(features[i])
+            for j in range(len(features))
+        ]
+        for i in range(len(features))
+    ]
+    a_i_matrix = [sum([x * y for x, y in zip(features[i], y_list)]) / len(features[i]) for i in range(len(features))]
+    my = sum(y_list) / len(y_list)
+
+    res_matrix = [
+        [1, *mx_list],
+        *[[mx_list[j], *a_ij_matrix[j]] for j in range(len(a_ij_matrix))]
+    ]
+    b = list(np.linalg.solve(res_matrix, [my, *a_i_matrix]))
+
+    return b
+
+
+def gen_norm_matrix(f_num, n=0, quadratic=False, y_list=None):
     k = 2 ** f_num - n
-    zero_factor = [1] * k
     test_reg = Regression(f_num, 2 ** f_num - f_num - n - 1, quadratic)
     x_variations = [list(item) for item in product([-1, 1], repeat=f_num)]
     matrix = []
@@ -178,23 +208,127 @@ def gen_norm_matrix(f_num, n=0, quadratic=False):
         row = list(map(mul, test_reg.terms_list))
         matrix.append(row)
 
-    return list(zip(zero_factor, *zip(*matrix)))
+    features = [
+        [-1.215, 0, 0],
+        [1.215, 0, 0],
+        [0, -1.215, 0],
+        [0, 1.215, 0],
+        [0, 0, -1.215],
+        [0, 0, 1.215],
+        [0, 0, 0]
+    ]
+
+    for i in range(len(features)):
+        test_reg.set_features(features[i])
+        row = list(map(mul, test_reg.terms_list))
+        matrix.append(row)
+
+    if y_list is not None:
+        for i in range(len(matrix)):
+            matrix[i].append(y_list[i])
+    return matrix
 
 
 def gen_nat_matrix(norm_matr):
-    k = len(norm_matr)
-    x = [[x_max_list[j] if norm_matr[i][j - 1] > 0 else x_min_list[j] for j in range(k - 1)] for i in range(k)]
-    return x
+    k = 8
+    f_num = 3
+    test_reg = Regression(f_num, 2 ** f_num - f_num - 1, True)
+    x_variations = [list(item) for item in product([-1, 1], repeat=f_num)]
+    x_variations_nat = [[x_min_list[j] if x_variations[i][j] < 0 else x_max_list[j] for j in range(3)] for i in
+                        range(len(x_variations))]
+    matrix = []
+    for i in range(k):
+        test_reg.set_features(x_variations_nat[i])
+        row = list(map(mul, test_reg.terms_list))
+        matrix.append(row)
+
+    features = [
+        [-1.215, 0, 0],
+        [1.215, 0, 0],
+        [0, -1.215, 0],
+        [0, 1.215, 0],
+        [0, 0, -1.215],
+        [0, 0, 1.215],
+        [0, 0, 0]
+    ]
+    nat_features = [[naturalize(x_min_list[i], x_max_list[i], plan_point[i]) for i in range(len(plan_point))] for
+                    plan_point in features]
+    for plan_point in nat_features:
+        test_reg.set_features(plan_point)
+        row = list(map(mul, test_reg.terms_list))
+        matrix.append(row)
+
+    return matrix
 
 
 def make_pretty_table(matr):
     table = PrettyTable()
-    header = ["x0", "x1", "x2", "x3", "x1 * x2", "x1 * x3", "x2 * x3", "x1 * x2 * x3", "x1 ^ 2", "x2 ^ 2", "x3 ^ 2"]
+    header = ["x1", "x2", "x3", "x1*x2", "x1*x3", "x2*x3", "x1*x2*x3", "x1 ^ 2", "x2 ^ 2", "x3 ^ 2", "Yav"]
+
     table.field_names = header[:len(matr[0])]
     for row in matr:
         table.add_row(row)
     return table
 
 
-norm_matrix = make_pretty_table(gen_norm_matrix(3, 0, True))
-print(norm_matrix)
+l = 1.215  # l - shoulder length
+m = 3  # number of experiments
+k = 3  # number of features
+N = 2 ** k + 2 * k + 1  # number of plan points
+norm_matrix = gen_norm_matrix(3, 0, True)
+nat_matrix = gen_nat_matrix(norm_matrix)
+
+while True:
+    print("m = {}".format(m))
+    y_values = [[uniform(y_min, y_max) for i in range(m)] for j in range(len(norm_matrix))]
+    y_average_values = [round(sum(yi) / len(yi), 3) for yi in y_values]
+
+    norm_matrix_table = make_pretty_table(gen_norm_matrix(3, 0, True, y_average_values))
+    nat_matrix_table = make_pretty_table(nat_matrix)
+
+    # -----------------------
+
+    b_list = calc_coeffs(nat_matrix, y_average_values)
+    test_regression = Regression(3, 4, True)
+    test_regression.set_coeffs(b_list)
+    new_y_values_correct = [test_regression.resp_func_val(x_list=nat_matrix[i][:3]) for i in range(len(nat_matrix))]
+    y_dispersion_list = [dispersion(y_average_values[i], y_values[i]) for i in range(len(y_values))]
+
+    # ------------ Cochren's criteria ------------
+    gp = max(y_dispersion_list) / sum(y_dispersion_list)
+    f1, f2 = m - 1, 2 ** k + 2 * k + 1
+    if gp < cohren(f1, f2):
+        break
+    m += 1
+    print("Dispersion is heterogeneous. Incrementing m...")
+
+# ------------ Student's criteria ------------
+f3 = f1 * f2
+t_t = student(f3)
+
+num_of_exp = 10
+disp_b = sum(y_dispersion_list) / num_of_exp
+disp_bs = disp_b / (num_of_exp * m)
+std_bs = disp_bs ** (1 / 2)
+beta_list = [beta_value(y_average_values, list(zip(*norm_matrix))[i]) for i in range(num_of_exp)]
+t_list = [abs(beta_list[i]) / std_bs for i in range(num_of_exp)]
+new_nat_coeffs = [b_list[i] if i < 1 or i > 7 else 0.0 for i in range(11)]
+
+# ------------- Fisher's criteria -------------
+d = len(new_nat_coeffs) - len([coeff for coeff in new_nat_coeffs if coeff != 0])
+f4 = N - d
+f_t = fisher(f3, f4)
+
+new_y_values = [test_regression.resp_func_val(x_list=plan_point[:3], coeff_list=new_nat_coeffs) for plan_point in
+                nat_matrix]
+disp_ad = (m / (N - d)) * sum([dispersion(y_average_values[i], new_y_values_correct) for i in range(k)])
+f_p = float(disp_ad / disp_b)
+adequacy = f_t > f_p
+
+print("Normalized orthogonal central composition plan:\n{}".format(norm_matrix_table))
+print("{}Naturalized orthogonal central composition plan:\n{}".format("\n" * 2, nat_matrix_table))
+print("Naturalized coefficients:" + (len(b_list) * " {:.4},").format(*b_list))
+print("Y values:" + (len(new_y_values) * " {:.4},").format(*new_y_values))
+print("New Y values:" + (len(new_y_values_correct) * " {:.4},").format(*new_y_values_correct))
+print("New coefficients:" + (len(new_nat_coeffs) * " {:.4},").format(*new_nat_coeffs))
+print("Adequacy of the regression equation: {}".format(adequacy))
--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,7 @@
+from functools import partial
+from scipy.stats import f, t
+
+
 def dispersion(aver_val, array):
     """
     Function for calculating dispersion of given array
@@ -22,3 +26,33 @@ def response_function(coeffs, variables):
     """
     y = coeffs[0] + sum([b * x for b, x in zip(coeffs[1:], variables)])
     return y
+
+
+def cohren(f1, f2, q=0.05):
+    fisher_value = f.ppf(q=1 - (q / f2), dfn=f1, dfd=(f2 - 1) * f1)
+    return fisher_value / (fisher_value + f2 - 1)
+
+
+def student(f3):
+    return partial(t.ppf, q=1 - 0.025)(df=f3)
+
+
+def fisher(f3, f4):
+    return partial(f.ppf, q=1 - 0.05)(dfd=f3, dfn=f4)
+
+
+def beta_value(y_av_arr, norm_plan_point):
+    """
+    Calculating beta-value:
+        Bj = 1 / N * sum(Yi, Xij), i = (1 -> K)
+        where:
+            N - number of plan points;
+            K - number of factors;
+            Yi - average value of Y for i row;
+            Xij - normalized value of X for i row j column;
+    :param y_av_arr: array of y average values
+    :param norm_plan_point:
+    :return: beta value for specified column
+    """
+    beta = sum(map(lambda y, x: y * x, y_av_arr, norm_plan_point)) / len(y_av_arr)
+    return beta
